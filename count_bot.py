781f4024c876387c49aabb1c9324720bd39d4a85
Fred Hsu <freddie.hsu@gmail.com>
2020-04-21 00:07:07 -0400

    Figured out how to silently ignore messages from unwanted channels

--- count_bot.py
+++ count_bot.py
@@ -6,19 +6,44 @@ from my_tokens import get_bot_token
 
 logging.basicConfig(level=logging.INFO)
 
-INVENTORY_CHANNEL = 'test-sandbox'
+INVENTORY_CHANNEL = 'sandbox'  # The bot only listens to text channels with this prefix or postfix, or DMs
+
+
+def fake_command_prefix_in_right_channel(_bot, message):
+    """
+    This is really pathetic. All "Checks" (command-specific or global) operate on the raise-an-exception basis.
+    If I want use Checks to prevent this bot from responding to all channels except for DM and inventory channels,
+    common words such as count, etc. will raise all sorts of exceptions and pollute log for no good reason.
+    A workaround is to fake a command prefix in incorrect channels to prevent these channels from being picked up.
+
+    These are meant to be passed into the :attr:`.Bot.command_prefix` attribute.
+    """
+    ch = message.channel
+    if ch.type == discord.ChannelType.private:
+        print('private')
+        return ''  # no command prefix means that comments from this DM channel will be matched and processed
+    elif ch.type != discord.ChannelType.text:
+        print('not text')
+        return '#fake-prefix-no-one-uses#'
+    elif ch.name.endswith(INVENTORY_CHANNEL) or ch.name.startswith(INVENTORY_CHANNEL):
+        print('sandbox')
+        return ''
+    print('wrong channel')
+    return '#fake-prefix-no-one-uses#'
+
 
 description = '''Keep count of current numbers of face shields in each person's possession until the next drop.'''
 bot = commands.Bot(
     description=description,
-    command_prefix='',          # No need for prefix. We only run the bot in special channels and in DM channels
-    case_insensitive=True,      # No need to be draconian with case
+    case_insensitive=True,  # No need to be draconian with case
+    command_prefix=fake_command_prefix_in_right_channel,
 
     help_command=commands.DefaultHelpCommand(
         no_category='Commands:',
         dm_help=True,
     ),
 )
+
 
 @bot.event
 async def on_ready():
@@ -28,13 +53,11 @@ async def on_ready():
     print('------')
 
 
-
-
-
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
+
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -48,10 +71,12 @@ async def roll(ctx, dice: str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
+
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
+
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
@@ -59,10 +84,12 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
+
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
 
 @bot.group()
 async def cool(ctx):
@@ -71,6 +98,7 @@ async def cool(ctx):
     """
     if ctx.invoked_subcommand is None:
         await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
 
 @cool.command(name='bot')
 async def _bot(ctx):
